5547e21d56aa3463d8a3d5a97f0a8f10ef95ea6b
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 06:53:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,6 @@
 def square_array(array)
-  numbers = [9,10,16,25]
+  numbers = [1,2,3]
   array.each { |new_numbers| puts "#{numbers} ** 2" }
 end
+numbers = [9,10,16,25]
+array.each { |new_numbers| puts "#{numbers} ** 2" }
