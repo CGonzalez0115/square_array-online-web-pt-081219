724d7446a1dc127509a26d093194bd7bf09df319
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 06:54:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,5 +2,6 @@ def square_array(array)
   numbers = [1,2,3]
   array.each { |new_numbers| puts "#{numbers} ** 2" }
 end
-numbers = [9,10,16,25]
-array.each { |new_numbers| puts "#{numbers} ** 2" }
+  numbers = [9,10,16,25]
+  array.each { |new_numbers| puts "#{numbers} ** 2" }
+end
